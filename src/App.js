2eb2b0ad1e0fb712839d31184f9924c550be19d6
joshuapawlik <joshawesome12@yahoo.com>
2018-04-24 17:36:43 -0500

    Sets up server with Node.js/Express and uses proxy on port 8080

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.sass';
 
 class App extends Component {
+
+
+  handleClick = () =>{
+    console.log('request inititated');
+    axios.get('/repos').then(()=>{
+      console.log('in .then')
+    }
+    )
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           {/* <img src={require('../public/github-512.png')} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to the Github Fetcher</h1>
+          <h1 className="App-title">Github Fetcher</h1>
         </header>
+        <div>
+         <h2 className='fetch-title'>Enter a Github Username to fetch repos:     </h2><div className='fetch-input'><input placeholder="Github UserName"></input><button onClick={this.handleClick}>Fetch</button></div>
+        </div>
       </div>
     );
   }
