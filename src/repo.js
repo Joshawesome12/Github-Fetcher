fb1221d6e1ccdb0b8c2499a2d931cbdb45c029ca
joshuapawlik <joshawesome12@yahoo.com>
2018-04-27 08:27:03 -0500

    Renders git image next to repo name as clickable link

--- src/repo.js
+++ src/repo.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 
 const Repo = (props) => (
-  <div>
+  <div className = 'repo'>
+    <a href={props.repo.html_url}>
+      <img className='gitImage' src={require('./resources/red-git.png')} alt='git'/>
+    </a>
     <h4 className="repoName" >{props.repo.name}</h4>
   </div>
 );
